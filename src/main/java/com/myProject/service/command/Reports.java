a6735dba63d150ea704fc1467a71fd28d9395e37
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-10-06 15:15:00 +0300

    Draft #43

--- src/main/java/com/myProject/service/command/Reports.java
+++ src/main/java/com/myProject/service/command/Reports.java
@@ -1,5 +1,6 @@
 package com.myProject.service.command;
 
+import com.myProject.service.employee.Employee;
 import com.myProject.service.exception.DaoException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
@@ -9,11 +10,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.myProject.util.Constants.PATH;
+
 public class Reports implements Command {
     private static final Logger logger = (Logger) LogManager.getLogger(Reports.class);
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws DaoException, ServletException, IOException {
         logger.info("--- Reports ---");
-        return null;
+        logger.info(req.getParameter("command"));
+        String role = ((Employee) req.getSession().getAttribute("employee")).getUser().getRole().getName();
+        if ("senior cashier".equals(role)) {
+            return PATH + "reports_senior_cashier.jsp";
+        }
+        if ("commodity expert".equals(role)) {
+            return PATH + "reports_commodity_expert.jsp";
+        }
+        return req.getHeader("referer");
     }
 }
--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -86,18 +86,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `cash_register`.`in_out`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cash_register`.`in_out` ;
-
-CREATE TABLE IF NOT EXISTS `cash_register`.`in_out` (
-  `value` VARCHAR(5) NOT NULL,
-  PRIMARY KEY (`value`),
-  UNIQUE INDEX `value_UNIQUE` (`value` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `cash_register`.`order`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `cash_register`.`order` ;
@@ -108,17 +96,12 @@ CREATE TABLE IF NOT EXISTS `cash_register`.`order` (
   `time` DATETIME NULL,
   `totalAmount` DECIMAL(15,2) NULL,
   `direction` VARCHAR(5) NOT NULL,
-  PRIMARY KEY (`id`, `user_id`, `direction`),
+  `reported` TINYINT NULL,
+  PRIMARY KEY (`id`, `user_id`),
   INDEX `fk_ordes_users1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_order_in_out1_idx` (`direction` ASC) VISIBLE,
   CONSTRAINT `fk_ordes_users1`
     FOREIGN KEY (`user_id`)
     REFERENCES `cash_register`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_order_in_out1`
-    FOREIGN KEY (`direction`)
-    REFERENCES `cash_register`.`in_out` (`value`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
