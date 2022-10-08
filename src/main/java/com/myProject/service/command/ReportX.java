116107b12e6bdc84490619d93414253a873e7340
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-10-08 16:40:01 +0300

    Draft #45 reports 5

--- src/main/java/com/myProject/service/command/ReportX.java
+++ src/main/java/com/myProject/service/command/ReportX.java
@@ -1,6 +1,7 @@
 package com.myProject.service.command;
 
 import com.myProject.dto.Report;
+import com.myProject.dto.ReportItem;
 import com.myProject.service.CashierManager;
 import com.myProject.service.employee.Employee;
 import com.myProject.service.exception.DaoException;
@@ -9,11 +10,15 @@ import org.apache.logging.log4j.core.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.font.PDType1CFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -46,37 +51,68 @@ public class ReportX implements Command {
     }
 
     private String createPDF(Report report) throws IOException {
+        logger.info("start PDF creating");
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
         document.addPage(page);
 
         int pageHeight = (int) page.getTrimBox().getHeight();
         int pageWidth = (int) page.getTrimBox().getWidth();
-        System.out.println("pageHeight: " + pageHeight + ", pageWidth: " + pageWidth);
 
         PDPageContentStream stream = new PDPageContentStream(document, page);
         stream.setLineWidth(1);
 
         int initX = 50;
-        int initY = pageHeight-50;
+        int initY = pageHeight-500;
         int cellHeight = 30;
         int cellWidth = 80;
-        int columnCount = 6;
-        int rowCount = 10;
-        for (int i = 0; i < rowCount; i++) {
-            for (int j = 0; j < columnCount; j++) {
-                stream.addRect(initX, initY, cellWidth, -cellHeight);
-                stream.beginText();
-                stream.newLineAtOffset(initX + 10, initY - cellHeight + 10);
-                stream.setFont(PDType1Font.TIMES_ROMAN, 14);
-                stream.showText("Cell #" + (i + 1) + ":" + (j + 1));
-                stream.endText();
-                initX += cellWidth;
-            }
-            initX = 50;
-            initY -= cellHeight;
+        stream.beginText();
+        PDFont font = PDType0Font.load(document, new FileInputStream("c:/windows/fonts/arial.ttf"), false);
+        stream.setFont(font, 14);
+        stream.setLeading(16.0f);
+        stream.showText("*** Report X ***");
+        stream.newLine();
+        stream.showText("----------------------------------------------------------");
+        stream.newLine();
+        stream.showText("Start date: " + report.getStartDate());
+        stream.newLine();
+        stream.showText("End date  : " + report.getEndDate());
+        stream.newLine();
+        stream.showText("Senior cashier: " + report.getSeniorCashier());
+        stream.newLine();
+        stream.showText("----------------------------------------------------------");
+        stream.newLine();
+        stream.showText(" Id " + "|" + " Product name " + "|" + " Unit " + "|" + " Quantity " + "|" + " Price " + "|" + " Amount ");
+        stream.newLine();
+        stream.showText("----------------------------------------------------------");
+        stream.newLine();
+        stream.endText();
+        for (ReportItem element : report.getList()) {
+            stream.beginText();
+            stream.showText(String.valueOf(element.getProductId()));
+            stream.newLineAtOffset(initX + 50, initY);
+            stream.showText(element.getProductName());
+            stream.newLineAtOffset(initX + 200, initY);
+            stream.showText(String.valueOf(element.getUnit()));
+            stream.newLineAtOffset(initX + 50, initY);
+            stream.showText(String.valueOf(element.getQuantity()));
+            stream.newLineAtOffset(initX + 100, initY);
+            stream.showText(String.valueOf(element.getPrice()));
+            stream.newLineAtOffset(initX + 100, initY);
+            stream.showText(String.valueOf(element.getAmount()));
+            stream.endText();
+            initY -= 30;
         }
-        stream.stroke();
+        stream.beginText();
+        stream.showText("----------------------------------------------------------");
+        stream.newLine();
+        stream.showText("Total quantity (kg): " + report.getKgTotal());
+        stream.newLine();
+        stream.showText("Total quantity (pcs): " + report.getPcsTotal());
+        stream.newLine();
+        stream.showText("Total amount: " + report.getAmountTotal());
+        stream.newLine();
+        stream.endText();
         stream.close();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
         LocalDateTime now = LocalDateTime.now();
