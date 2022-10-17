58d7adbb68b9e7c9d56f2849256d78711c2b587d
Oleksiy <oleksiymiasnikov@gmail.com>
2022-10-17 16:30:06 +0300

    Draft #53 start making send email

--- src/main/java/com/myProject/service/command/SendReport.java
+++ src/main/java/com/myProject/service/command/SendReport.java
@@ -6,14 +6,14 @@ import com.myProject.service.exception.DaoException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -28,18 +28,65 @@ public class SendReport implements Command {
         logger.info("SEND_REPORT_COMMAND executed");
         Employee employee = (Employee) req.getSession().getAttribute("Employee");
 
-        Properties properties = new Properties();
-        Session session = Session.getInstance(properties, null);
+
+        final String username = "oleksiymiasnikov@gmail.com";
+        final String password = "CashRegisterEPAM2022";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("oleksiymiasnikov@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("oleksiymiasnikov@gmail.com")
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        /*Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.mailtrap.io");
+        prop.put("mail.smtp.port", "25");
+        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+        Session session = Session.getInstance(prop, null);
         MimeMessage message = new MimeMessage(session);
         try {
             message.setFrom(new InternetAddress("alex_1@ukr.net")); //employee.getUser().getEmail()
             message.addRecipient(Message.RecipientType.TO, new InternetAddress("alex_1@ukr.net"));
             message.setSubject("report");
             message.setText("This is the email body");
+            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+            attachmentBodyPart.attachFile(new File("c:\\1.txt"));
         } catch (MessagingException e) {
             throw new AppException(e);
         }
-
+*/
         return req.getHeader("referer");
     }
 }
