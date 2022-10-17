2e83ad8955a29ce98b43b7e7ff978556ef5755d8
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-10-17 17:40:59 +0300

    Draft #53 emailing

--- src/main/java/com/myProject/service/command/SendReport.java
+++ src/main/java/com/myProject/service/command/SendReport.java
@@ -10,6 +10,7 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,17 +27,15 @@ public class SendReport implements Command {
     public String execute(HttpServletRequest req, HttpServletResponse resp)
             throws DaoException, ServletException, IOException {
         logger.info("SEND_REPORT_COMMAND executed");
-        Employee employee = (Employee) req.getSession().getAttribute("Employee");
-
-
+        Employee employee = (Employee) req.getSession().getAttribute("employee");
+        String filePDF = req.getServletContext().getRealPath((String) req.getSession().getAttribute("pdf"));
         final String username = "oleksiymiasnikov@gmail.com";
-        final String password = "CashRegisterEPAM2022";
-
+        final String password = "fasgvwhmekiknkam";
         Properties prop = new Properties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.port", "465");
         prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         Session session = Session.getInstance(prop,
                 new javax.mail.Authenticator() {
@@ -44,49 +43,26 @@ public class SendReport implements Command {
                         return new PasswordAuthentication(username, password);
                     }
                 });
-
         try {
-
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("oleksiymiasnikov@gmail.com"));
             message.setRecipients(
                     Message.RecipientType.TO,
-                    InternetAddress.parse("oleksiymiasnikov@gmail.com")
-            );
-            message.setSubject("Testing Gmail TLS");
-            message.setText("Dear Mail Crawler,"
-                    + "\n\n Please do not spam my email!");
-
+                    InternetAddress.parse("oleksiymiasnikov@gmail.com"));
+            message.setSubject("Report");
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText("Mail Body");
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            attachmentPart.attachFile(new File(filePDF));
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            multipart.addBodyPart(attachmentPart);
+            message.setContent(multipart);
             Transport.send(message);
-
             System.out.println("Done");
-
         } catch (MessagingException e) {
-            e.printStackTrace();
+            throw new AppException("Unable to send email", e);
         }
-
-
-
-
-        /*Properties prop = new Properties();
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.mailtrap.io");
-        prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
-        Session session = Session.getInstance(prop, null);
-        MimeMessage message = new MimeMessage(session);
-        try {
-            message.setFrom(new InternetAddress("alex_1@ukr.net")); //employee.getUser().getEmail()
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress("alex_1@ukr.net"));
-            message.setSubject("report");
-            message.setText("This is the email body");
-            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
-            attachmentBodyPart.attachFile(new File("c:\\1.txt"));
-        } catch (MessagingException e) {
-            throw new AppException(e);
-        }
-*/
         return req.getHeader("referer");
     }
 }
